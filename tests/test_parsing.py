1f27b70663e2b76e72ee3f963d28f2ff859e55ca
Haydn King <hjking734@gmail.com>
2017-08-03 11:52:06 +0100

    Add rate_equation tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,7 +1,7 @@
 
 from context import pysim
 
-import unittest, os.path
+import unittest, os.path, numpy as np, numpy.testing as npt
 
 test_data = os.path.join(os.path.dirname(__file__), "data/")
 
@@ -18,6 +18,41 @@ class ParsingTestSuite(unittest.TestCase):
                                         "{}.expected.model".format(s))) as f:
                 expected = f.read()
             self.assertEqual(expected, str(model), "in test {}".format(s))
+
+class RateTests(unittest.TestCase):
+    def setUp(self):
+         self.model = pysim.ODEModel.fromFile(os.path.join(test_data, 
+                                                           "ratetest.model"))
+
+    def test_fw_reaction(self):
+        fn = self.model.reactions[0].getRateEquation()
+        exp = np.array([-1.0, 1.0, 0.0])
+        tests = [([1,1,1], 1.0*exp),
+                 ([1,2,2], 1.0*exp),
+                 ([7,2,2], 7.0*exp),]
+        self._helper(fn, tests)
+
+    def test_reversible_reaction(self):
+        fn = self.model.reactions[1].getRateEquation()
+        exp = np.array([-1.0, 1.0, 0.0])
+        tests = [([1,1,1],  0.0*exp),
+                 ([1,2,2], -1.0*exp),
+                 ([7,2,2],  5.0*exp),]
+        self._helper(fn, tests)
+
+    def test_MM_reaction(self):
+        fn = self.model.reactions[2].getRateEquation()
+        exp = np.array([-1.0, 1.0, 0.0])
+        tests = [([1,1,1], 0.5*exp),
+                 ([1,2,2], 1.0*exp),
+                 ([7,2,2], 2.8*exp),]
+        self._helper(fn, tests)
+
+    def _helper(self, fn, tests):
+        for i,(y, resp) in enumerate(tests):
+            npt.assert_allclose(fn(y), resp, err_msg="case {}".format(i))
+
+
 
 if __name__ == "__main__":
     unittest.main()
